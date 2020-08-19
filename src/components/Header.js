4f1d0f7d2869bdf6b7128980acb955942ca75016
Monika N <monika258@gmail.com>
2020-08-19 20:51:10 +0200

    header done

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,54 @@
-import React from "react";
+import React, {useState} from "react";
+import {Link as LinkScroll} from 'react-scroll';
+import classNames from 'classnames';
+
 
 const Header = () => {
+    const [toggleOpen, setToggleOpen] = useState(false);
+
+    const handleButtonClick = () => {
+        setToggleOpen(!toggleOpen);
+    };
+
     return (
-        <div className='home'>
-            Header
+        <div className='header'>
+            <nav className='navbar navbar-expand-lg navbar-dark bg-dark sticky-top header__navbar' >
+                <a target='_blank' rel='noopener noreferrer' href='https://github.com/MoNo258'
+                   className='navbar-brand header__brand'>
+                    MoNo258
+                </a>
+                <button className="navbar-toggler header__button" type="button" data-toggle="collapse"
+                        data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                        aria-expanded="false"
+                        aria-label="Toggle navigation"
+                        onClick={handleButtonClick}>
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div id='navbarSupportedContent'
+                    className={classNames('collapse navbar-collapse justify-content-end header__list',{'show':toggleOpen})}
+                >
+                    <ul className='navbar-nav mr-auto header__list--ul'>
+                        <li className='nav-item header__item'>
+                            <LinkScroll to='heroHome' className='nav-link header__link'
+                                        spy={true} smooth={true} offset={0} duration={500}>
+                                Start
+                            </LinkScroll>
+                        </li>
+                        <li className='nav-item  header__item'>
+                            <LinkScroll to='projects' className='nav-link header__link'
+                                        spy={true} smooth={true} offset={0} duration={500}>
+                                Projects
+                            </LinkScroll>
+                        </li>
+                        <li className='nav-item  header__item'>
+                            <LinkScroll to='contact' className='nav-link header__link'
+                                        spy={true} smooth={true} offset={0} duration={500}>
+                                Contact me
+                            </LinkScroll>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
         </div>
     )
 };
--- src/components/Home.js
+++ src/components/Home.js
@@ -5,15 +5,21 @@ import Projects from "./Projects";
 import Contact from "./Contact";
 import Footer from "./Footer";
 
+
 const Home = () => {
     return (
         <div className='home'>
-            <Header />
-            <HeroHome />
-            <Projects />
-            <Contact />
-            <Footer />
-            <div className='svgIcons'>Icons made by <a href="https://www.flaticon.com/authors/dave-gandy" title="Dave Gandy">Dave Gandy</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
+            <Header id='header' />
+            <HeroHome id='heroHome' />
+            <Projects id='projects' />
+            <Contact id='contact' />
+            <Footer id='footer' />
+            <div className='svgIconsInfo'>
+                Icons made by <span>Dave Gandy</span> from
+                <a target='_blank' rel='noopener noreferrer' href='https://www.flaticon.com/' title='Flaticon'>
+                    www.flaticon.com
+                </a>
+            </div>
         </div>
     )
 };
