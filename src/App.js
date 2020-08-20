12a28bd195d1a297e74661abdfe0a66077b8c4b6
Monika N <monika258@gmail.com>
2020-08-20 09:51:23 +0200

    projects part layout

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './scss/main.scss';
 import {HashRouter as Router, Switch, Route} from "react-router-dom";
 import Home from "./components/Home";
+import Project from "./components/Project";
 
 function App() {
   return (
     <Router>
         <Switch>
             <Route exact path='/' component={Home} />
+            <Route path='/project' component={Project} />
         </Switch>
     </Router>
   );
---
+++ src/components/Project.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Project = ({id}) => {
+    return (
+        <div className='project' id={id}>
+            project's layout
+        </div>
+    )
+};
+
+export default Project;
