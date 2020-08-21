12e0618279a9bb8eb87dae7f9b558fb6e10524d6
Monika N <monika258@gmail.com>
2020-08-21 16:42:15 +0200

    build version after layout preparation

---
+++ precache-manifest.d77aed433abddc97e2bc16f27573c597.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce3d7dc88f3394c09dc8415a8b0dd862",
+    "url": "./index.html"
+  },
+  {
+    "revision": "e4bd70a445d11188feae",
+    "url": "./static/css/main.9d302625.chunk.css"
+  },
+  {
+    "revision": "367f20610df8bb74be0a",
+    "url": "./static/js/2.82da146b.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "./static/js/2.82da146b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e4bd70a445d11188feae",
+    "url": "./static/js/main.29820e3e.chunk.js"
+  },
+  {
+    "revision": "610a38b7b5a7cb7b2d5e",
+    "url": "./static/js/runtime-main.916853f0.js"
+  },
+  {
+    "revision": "001787b1579b97b03d38f5700df0f26f",
+    "url": "./static/media/ChatApp.001787b1.JPG"
+  },
+  {
+    "revision": "e9964363436222132f9974fe780ebd40",
+    "url": "./static/media/attachment.e9964363.svg"
+  },
+  {
+    "revision": "14190820d1f9c4cb456bbd2dbad9af96",
+    "url": "./static/media/blackTag.14190820.svg"
+  },
+  {
+    "revision": "400c4dbf960c3c8fa0a5374faf6e2485",
+    "url": "./static/media/envelope.400c4dbf.svg"
+  },
+  {
+    "revision": "628d0f74f1a9052e24e3cba1b57d9410",
+    "url": "./static/media/exchange_rates_board.628d0f74.JPG"
+  },
+  {
+    "revision": "8f8edc5fb64b9bd436834dc926ac992c",
+    "url": "./static/media/github.8f8edc5f.svg"
+  },
+  {
+    "revision": "154629f25d417c8c0a4ac3a83707979a",
+    "url": "./static/media/linkedin.154629f2.svg"
+  },
+  {
+    "revision": "37bf4a5dda3edd72450cfa0fde38a78d",
+    "url": "./static/media/potluck_party_planner.37bf4a5d.JPG"
+  },
+  {
+    "revision": "2db67cdc04d1b543a27933a8204419f1",
+    "url": "./static/media/rocket.2db67cdc.svg"
+  },
+  {
+    "revision": "e24efa9c1af840f3988b6c7c72d496ec",
+    "url": "./static/media/share_because_you_care.e24efa9c.JPG"
+  },
+  {
+    "revision": "ce27ac0af18c0a11efc2ed2e125036a6",
+    "url": "./static/media/tree_fog.ce27ac0a.jpg"
+  },
+  {
+    "revision": "fee678ca416673ebff1e08ec5b18d54e",
+    "url": "./static/media/zaplanuj_jedzonko.fee678ca.JPG"
+  }
+]);
