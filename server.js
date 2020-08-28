dafa53e57ff59d66fec68a56237d640d5d7424ab
Monika N <monika258@gmail.com>
2020-08-28 22:09:05 +0200

    removed not used require

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-require('babel-polyfill');
+// require('babel-polyfill');
 const mailer = require('./src/server/mailer');
 const express = require('express');
 const bodyParser = require('body-parser');
